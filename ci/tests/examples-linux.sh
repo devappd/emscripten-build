118a596904aad677d8a728f326aa6bea0ee6fe26
mazmazz <mar.marcoz@outlook.com>
2020-11-25 01:16:34 -0500

    New Travis-CI tests

---
+++ ci/tests/examples-linux.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+echo -e "
+########################################################################
+# tests\examples-linux.sh
+#
+# For each example project, install emscripten-build, then build,
+# then check if *.js and *.wasm exist in ./dist
+########################################################################
+"
+
+echo -e "
+########################################################################
+# Setting up tests...
+########################################################################
+"
+
+failed=0
+passCount=0
+
+declare -a examples=(
+    "Example-01-HelloWorld"
+    "Example-04-CMake"
+    "Example-05-Library"
+)
+
+cd ~
+
+git clone https://github.com/marcolovescode/emscripten-npm-examples ./emscripten-npm-examples
+
+cd ./emscripten-npm-examples
+repoRoot=$PWD
+
+for example in "${examples[@]}"
+do
+
+    echo -e "
+########################################################################
+# BUILD TEST - $example
+########################################################################
+"
+
+    cd "$repoRoot/$example"
+    rm -rf ./dist/*
+    
+    npm install --emsdk="$HOME/emsdk"
+    npm run build
+
+    # https://stackoverflow.com/a/33891876
+    cd ./dist
+    countJs=$(ls 2>/dev/null -Ubad1 -- *.js | wc -l)
+    countWasm=$(ls 2>/dev/null -Ubad1 -- *.wasm | wc -l)
+
+    echo ""
+    echo "Build Output"
+    ls
+
+    if [ $countJs -eq 1 ] && [ $countWasm -eq 1 ]
+    then
+        message="BUILD PASSED!"
+        let passCount++
+    else
+        message="BUILD FAILED!"
+        let failed++
+    fi
+
+echo -e "
+########################################################################
+# $message $example
+########################################################################
+"
+
+done
+
+if [ $failed -gt 0 ]; then
+
+    echo -e "
+########################################################################
+# BUILD FAILED! (PASSED: $passCount/${#examples[@]})
+# Check above to see which build failed.
+########################################################################
+"
+
+    # Fail the Travis build
+    return 1
+
+else
+
+    echo -e "
+########################################################################
+# ALL BUILDS PASSED! ($passCount/${#examples[@]})
+########################################################################
+"
+
+    return 0
+fi
---
+++ ci/tests/examples-win.ps1
@@ -0,0 +1,90 @@
+Write-Output @"
+########################################################################
+# tests\examples-win.ps1
+#
+# For each example project, install emscripten-build, then build,
+# then check if *.js and *.wasm exist in ./dist
+########################################################################
+"@
+
+Write-Host @"
+########################################################################
+# Setting up tests...
+########################################################################
+"@
+
+# Windows can't find NPM without the full path.
+#
+# At this point, Travis has called NVS and added the Node location
+# as the first element in PATH. The PATH is consistent between
+# PowerShell and Bash.
+$nodePath = "$env:PATH".Split(";")[0]
+
+$failed = $false
+$passCount = 0
+
+$examples = @(
+    "Example-01-HelloWorld",
+    "Example-04-CMake",
+    "Example-05-Library"
+)
+
+Start-Process -FilePath 'git' -ArgumentList ('clone','https://github.com/marcolovescode/emscripten-npm-examples') -Wait -NoNewWindow
+
+Set-Location .\emscripten-npm-examples
+$repoRoot = (Get-Location).Path
+
+foreach ($example in $examples) {
+    Write-Output @"
+########################################################################
+# BUILD TEST - $example
+########################################################################
+"@
+
+    Set-Location "$repoRoot\$example"
+    Remove-Item -Recurse -Force .\dist\*
+    
+    # Node 11.x does not have npm.ps1, so run CMD
+    Start-Process -FilePath 'cmd.exe' -ArgumentList ("/c", "$nodePath\npm.cmd", 'install', '--emsdk="C:\emsdk"') -Wait -NoNewWindow
+    Start-Process -FilePath 'cmd.exe' -ArgumentList ("/c", "$nodePath\npm.cmd", 'run', 'build') -Wait -NoNewWindow
+
+    $countJs = (Get-ChildItem .\dist\*.js | measure).Count
+    $countWasm = (Get-ChildItem .\dist\*.wasm | measure).Count
+    $hasArtifacts = ($countJs -eq 1 -and $countWasm -eq 1)
+
+    Write-Output ""
+    Write-Output "Build Output"
+    Get-ChildItem .\dist
+
+    If ($hasArtifacts -eq $true) {
+        $message = "BUILD PASSED!"
+        $passCount++
+    } else {
+        $message = "BUILD FAILED!"
+        $failed = $true
+    }
+
+    Write-Output @"
+########################################################################
+# $message $example
+########################################################################
+"@
+}
+
+If ($failed -eq $true) {
+    Write-Output @"
+########################################################################
+# BUILD FAILED! ($passCount/$($examples.Count))
+# Check above to see which build failed.
+########################################################################
+"@
+    # fail Travis build
+    exit 1
+} Else {
+    Write-Output @"
+########################################################################
+# ALL BUILDS PASSED! ($passCount/$($examples.Count))
+########################################################################
+"@
+    exit 0
+}
